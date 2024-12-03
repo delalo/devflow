626f2438ef63fb506ca752c31fb0e7343a4b686e
delalo <bernardahlijah3@gmail.com>
2024-12-03 14:58:12 +0000

    add auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+      <div>
+        <h1 className=" h1-bold text-primary-500 ">
+          {" "}
+          Next.js 15 we &apos; re here{" "}
+        </h1>
+      </div>
+    );
+  }
+  
